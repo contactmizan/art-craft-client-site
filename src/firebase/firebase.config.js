2925b368a6b075a6d275f912f12bf5ac4e145b33
contactmizan <contactmizan1206@gmail.com>
2024-11-28 02:02:35 +0600

    set up firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC0hC4v_krsigT2OqekMJcMUWa76RiFWlQ",
+    authDomain: "art-craft-store-1c961.firebaseapp.com",
+    projectId: "art-craft-store-1c961",
+    storageBucket: "art-craft-store-1c961.firebasestorage.app",
+    messagingSenderId: "97563999788",
+    appId: "1:97563999788:web:0e4a6922c2229c27fb5b89"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
